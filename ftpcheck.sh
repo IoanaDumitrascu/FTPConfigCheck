5e429a9f7e7579b817600ab33c040c0f5bbe4698
Ioana Dumitrascu <ioana.dumitrascu95@yahoo.com>
2026-01-11 19:13:17 +0200

    Commit 2: permisiunile si autorul, verificam persmisunile pt others sa fie sigure(nu drept de scriere)

--- ftpcheck.sh
+++ ftpcheck.sh
@@ -12,3 +12,24 @@ if [ ! -f "$file" ]; then
 	echo "EROARE! Fisier inexistent"
 	exit 1
 fi 
+#am verificat ca numele fisierului este scris si ca acesta exista
+#acum incepem analiza fisierului
+echo "analiza fisier: $file"
+
+#verificam permisiunile si proprietarul
+full_ls=$(ls -ld "$file")
+perm="${full_ls:0:10}"
+owner=$(ls -ld "$file" | awk '{print $3}')
+
+echo "permisiuni: $perm"
+echo "proprietar: $owner"
+
+#verificam permisiunile pt 'others'- ultimlele 3 caractere
+others_perm="${perm:8:3}"
+#daca au drept de scriere, fisierul nu mai este sigur deoarece oricine poate aduce orice modificare
+if echo "$others_perm" | grep -q "w"; then
+	echo "ATENTIE! permisiuni de scriere nesigure"
+else
+	echo "OK! permisiuni de scriere sigure"
+fi
+

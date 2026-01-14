f361418529d90a439d094d2da0c7dfad48a3380c
IoanaDumitrascu <ioana.dumitrascu95@yahoo.com>
2026-01-14 20:07:13 +0200

    pas2: script de la lab 5 modificat , functie shell

--- ex3.sh
+++ ex3.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+
+verif_ip(){
+
+	local ip_1=$1
+	local nume_host=$2
+
+	local ip_2=$(nslookup "$nume_host" | grep "Adress:" | tail -n 1 | awk '{print $2}')
+	if [[ -n "$ip_2" && "ip_1" != "ip_2" ]]; then
+		echo "Bogus IP pt $nume_host"
+	fi
+}
+
 cat /etc/hosts | while read -r ip nume restul; do
-	if [[ -z "$ip" || "$ip" == "#"* || "$nume" == "localhost" ]] ; then
-		continue
-	fi
-	ip_bun=$(nslookup "$nume" | grep "Address:" | tail -n 1 | awk '{print $2}' | xargs)
-	if [[ -n "$ip_bun" && "$ip" != "ip_bun" ]]; then
-		echo "Bogus IP for $nume in /etc/hosts!"
-		echo "In fisier: $ip"
-		echo "In DNS: $ip_bun"
-	fi
+	if [[ -z "$ip" || "$ip" == "#"* || "$nume" == "localhost" ]] && continue
+	
+	verif_ip "$ip" "$nume"
 done
